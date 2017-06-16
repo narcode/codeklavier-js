4eeb0b38deabe83b17e0be8dcc2f761c0334cf36
Narcode B <f.e.l.i.p.e@icloud.com>
2017-06-16 14:59:25 +0200

    type string on motif

--- mottipets/mottipets.js
+++ mottipets/mottipets.js
@@ -65,6 +65,15 @@ chromatic.forEach( (elem)=>{
        motifs.splice(index,1);
      }
    });
+
+robotjs.typeString("~snippet2 = Tdef(\snippet2, {
+loop{
+Ndef(\pulse2, {SinOsc.ar(456*LFTri.kr(88).range(300, ~topR)) * EnvGen.kr(Env.perc) * ~amp1}).play(0,2);
+
+(1/~tremolo).wait;
+}
+  }).play;");
+   
 }
 
  });
