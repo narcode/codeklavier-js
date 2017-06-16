b1d6790581449c2b0af6abdffb7831391bc0e8a8
Narcode B <f.e.l.i.p.e@icloud.com>
2017-06-16 16:53:52 +0200

    debug

--- mottipets/mottipets.js
+++ mottipets/mottipets.js
@@ -90,7 +90,7 @@ premotifmem.memorize(msg[1], chromatic.length);
    var prememtest = prememString.match(chromaticString);
   //  var match = regpattern.exec(motifsString);
 
-console.log(prememString);
+console.log("PREMEM -> " + prememString);
 
    if (regtest != null) {
    if (regtest.length > 0) {
