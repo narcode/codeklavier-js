c00368906a82830d0ca41f8f5ebbd25c26632fab
Narcode B <f.e.l.i.p.e@icloud.com>
2017-06-16 15:04:59 +0200

    test special chars string

--- mottipets/mottipets.js
+++ mottipets/mottipets.js
@@ -66,7 +66,13 @@ chromatic.forEach( (elem)=>{
      }
    });
 
-robot.typeString('{}');
+robot.typeString('~snippetest special charst2 = Tdef(\snippet2, {
+loop{
+Ndef(\pulse2, {SinOsc.ar(456*LFTri.kr(88).range(300, ~topR)) * EnvGen.kr(Env.perc) * ~amp1}).play(0,2);
+
+(1/~tremolo).wait;
+}
+  }).play;');
 
 }
 
