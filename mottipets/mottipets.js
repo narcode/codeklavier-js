015bc45d94d56706b6fbcec57c13b9abbacea04d
Narcode B <f.e.l.i.p.e@icloud.com>
2017-06-19 18:50:04 +0200

    mottipets listening to piano sections

--- mottipets/mottipets.js
+++ mottipets/mottipets.js
@@ -27,7 +27,10 @@ var chromatic = [ 30, 35, 36, 37, 38, 39, 40, 41, 42, 43, 42, 41, 40, 39, 38, 37
 // var motifs = [ 30, 35, 36, 37, 38, 39, 40, 41, 42, 43, 42, 41, 40, 39, 38, 37, 36, 35];
 var pianosectons = [47, 78, 108];
 
+var miniM1L = [36,43,44,39,38];
 var miniM1M = [60,67,68,63,62];
+var miniM1H = [84,91,92,87,86];
+
 
 var ignore = false;
 
@@ -67,20 +70,24 @@ function countNotes(array, note) {
 
 function minimotifSearch(array, elementsArray) {
   var itemcount = 0;
+  var itemindex = [];
   for (i=0; i<elementsArray.length; i++) {
 if (array.indexOf(elementsArray[i]) > -1) {
-itemcount++
-console.log("item count: " + itemcount);
+itemcount++;
+itemindex.push(array.indexOf(elementsArray[i]));
+// console.log("item count: " + itemcount);
 }
 
 if (itemcount == elementsArray.length) {
-  console.log("motif present");
-}
-
+  // console.log("motif present -> " + itemindex)
+if (itemindex[0] < itemindex[1] && itemindex[1] < itemindex[2]
+&& itemindex[2] < itemindex[3] ** itemindex[3] < itemindex[4]) {
+  console.log("good order")
+  return true
   }
 }
-
-minimotifSearch([1,2,3,4,5,6,7], [2,19,4]);
+  }
+}
 
 var memory = new Mem4block();
 var intervalmem = new Mem4block();
@@ -91,7 +98,13 @@ var minimotifsM = new Mem4block();
 var minimotifsH = new Mem4block();
 
 // counters:
-var mmotifcount = 0;
+var mmotifcountL = 0;
+var mmotifcountM = 0;
+var mmotifcountH = 0;
+
+// map switches
+var lMap = false;
+
 
  // on message write to a stream
  input.on('message', function(deltaTime, msg) {
@@ -105,32 +118,61 @@ premotifmem.memorize(msg[1], chromatic.length*1);
 }
 
 if (msg[1] <= pianosectons[0]) {
-minimotifsL.memorize(msg[1], 20);
+minimotifsL.memorize(msg[1], 20, true, 'L ->');
 } else if (msg[1] <= pianosectons[1]) {
-minimotifsM.memorize(msg[1], 20);
+minimotifsM.memorize(msg[1], 20, true, 'M ->');
 } else if (msg[1] > pianosectons[1]) {
-minimotifsH.memorize(msg[1], 20);
+minimotifsH.memorize(msg[1], 20, true, 'H ->');
 }
 
    } // fi
 
 // check if notes exists:
-if (minimotifsM.memory.includes(miniM1M[0]) && minimotifsM.memory.includes(miniM1M[1])
-&& minimotifsM.memory.includes(miniM1M[2]) && minimotifsM.memory.includes(miniM1M[3]) &&
-minimotifsM.memory.includes(miniM1M[4])) {
-  // check if they are in the right order:
-if (minimotifsM.memory.indexOf(miniM1M[0]) < minimotifsM.memory.indexOf(miniM1M[1]) &&
-minimotifsM.memory.indexOf(miniM1M[1]) < minimotifsM.memory.indexOf(miniM1M[2]) &&
-minimotifsM.memory.indexOf(miniM1M[2]) < minimotifsM.memory.indexOf(miniM1M[3]) &&
-minimotifsM.memory.indexOf(miniM1M[3]) < minimotifsM.memory.indexOf(miniM1M[4])) {
-  mmotifcount++;
-  if (mmotifcount === 1) {
-  console.log("motif mapped! --> " + mmotifcount + ' times...');
-  robot.typeString('[\\pulse, \\pulse2, \\pulse3, \\pulse4, \\pulse5, \\pulse6].do{|i| Ndef(i).map(\\pitch, Ndef(\\krm1));}');
-  robot.keyTap('enter', 'shift'); robot.keyTap('enter');
-    }
+// if (minimotifsM.memory.includes(miniM1M[0]) && minimotifsM.memory.includes(miniM1M[1])
+// && minimotifsM.memory.includes(miniM1M[2]) && minimotifsM.memory.includes(miniM1M[3]) &&
+// minimotifsM.memory.includes(miniM1M[4])) {
+//   // check if they are in the right order:
+// if (minimotifsM.memory.indexOf(miniM1M[0]) < minimotifsM.memory.indexOf(miniM1M[1]) &&
+// minimotifsM.memory.indexOf(miniM1M[1]) < minimotifsM.memory.indexOf(miniM1M[2]) &&
+// minimotifsM.memory.indexOf(miniM1M[2]) < minimotifsM.memory.indexOf(miniM1M[3]) &&
+// minimotifsM.memory.indexOf(miniM1M[3]) < minimotifsM.memory.indexOf(miniM1M[4])) {
+//   mmotifcount++;
+//   if (mmotifcount === 1) {
+//   console.log("motif mapped! --> " + mmotifcount + ' times...');
+  // robot.typeString('[\\pulse, \\pulse2, \\pulse3, \\pulse4, \\pulse5, \\pulse6].do{|i| Ndef(i).map(\\pitch, Ndef(\\krm1));}');
+  // robot.keyTap('enter', 'shift'); robot.keyTap('enter');
+//     }
+//   }
+// }
+
+// Lowregister:
+if (minimotifSearch(minimotifsL.memory, miniM1L)) {
+  mmotifcountL++;
+    if (mmotifcountL === 1) {
+  console.log("motif L mapped! --> " + mmotifcountL + ' times...');
+lMap = true;
   }
 }
+
+if (minimotifSearch(minimotifsM.memory, miniM1M)) {
+  mmotifcountM++;
+  if (mmotifcountM === 1) {
+  console.log("motif M mapped! --> " + mmotifcountM + ' times...');
+  robot.typeString('[\\pulse, \\pulse2, \\pulse3, \\pulse4, \\pulse5, \\pulse6].do{|i| Ndef(i).map(\\pitch, Ndef(\\krm1));}');
+  robot.keyTap('enter', 'shift'); robot.keyTap('enter');
+  }
+}
+
+if (minimotifSearch(minimotifsH.memory, miniM1H)) {
+  mmotifcountH++;
+  if (mmotifcountH === 1) {
+  console.log("motif H mapped! --> " + mmotifcountH + ' times...');
+  robot.typeString('[\\pulse, \\pulse2, \\pulse3, \\pulse4, \\pulse5, \\pulse6].do{|i| Ndef(i).map(\\fx, Ndef(\\krm3));}');
+  robot.keyTap('enter', 'shift'); robot.keyTap('enter');
+  }
+}
+
+
 
   //  motifsString = motifs.join();
   motifsString = motifmem.memory.join();
@@ -149,7 +191,7 @@ minimotifsM.memory.indexOf(miniM1M[3]) < minimotifsM.memory.indexOf(miniM1M[4]))
    if (regtest.length > 0) {
    console.log('true! -> ' + regtest);
    console.log('length -> ' + chromatic.length);
-   robot.typeString('~snippet2 = Tdef(\\snippet2, { loop{ Ndef(~name.next, {|pitch=400| SinOsc.ar(456*LFTri.kr(88).range(300, pitch)) * EnvGen.kr(Env.perc) * ~amp1}).play(0,2);(1/1).wait;}}).play;');
+   robot.typeString('~snippet2 = Tdef(\\snippet2, {|ev| loop{ Ndef(~name.next, {|pitch=400,fx=88| SinOsc.ar(456*LFTri.kr(fx).range(300, pitch)) * EnvGen.kr(Env.perc) * ~amp1}).play(0,2);(1/ev.rit).wait;}}).play;');
    robot.keyTap('enter', 'shift'); robot.keyTap('enter');
    // ignore or listen to motif based on delta time?
  ignore = true;
@@ -204,8 +246,19 @@ if (intervalsum == 0) {
 // console.log("no change...");
 } else {
 // console.log("new interval...");
-robot.typeString('~tremolo = ' + interval);
-robot.keyTap('enter', 'shift'); robot.keyTap('enter');
+if (msg[1] <= pianosectons[1] && msg[1] > pianosectons[0]) {
+  robot.typeString('~tremoloM = ' + interval);
+  robot.keyTap('enter', 'shift'); robot.keyTap('enter');
+} else if (msg[1] <= pianosectons[0]) {
+  if (lMap === true) {
+  robot.typeString('Tdef(\\snippet2).set(\\rit, ' + interval + ' )');
+  robot.keyTap('enter', 'shift'); robot.keyTap('enter');
+  }
+} else if (msg[1] > pianosectons[1]) {
+  robot.typeString('~tremoloH = ' + interval);
+  robot.keyTap('enter', 'shift'); robot.keyTap('enter');
+}
+
 }
 // robot.typeString('~tremolo = ' + interval);
     } // > 0 fi=
@@ -213,22 +266,3 @@ robot.keyTap('enter', 'shift'); robot.keyTap('enter');
 }
 
  });
-
-/// tests debug:
-// motifsString = motifs.join();
-// chromaticString = chromatic.join();
-// regpattern = new RegExp(chromaticString);
-//
-// console.log("string: " + motifsString);
-// console.log("pattern: " + regpattern);
-//
-// var regtest = motifsString.match(chromaticString);
-//
-// if (regtest != null) {
-// if (regtest.length > 0) {
-// console.log('true!');
-//   }
-// }
-
-
-// 1) listen to the loop
