0a71ed98c9885bb8a9b946bc7753643ed07eb6ff
Narcode B <f.e.l.i.p.e@icloud.com>
2017-07-01 16:01:12 +0200

    note offs out

--- mottipets/mottipets.js
+++ mottipets/mottipets.js
@@ -162,7 +162,7 @@ motifmem.memorize(msg[1], 20, true, 'Big mama -> ');
 
 if (msg[1] >= chromatic[0] && msg[1] < chromatic[chromatic.length-1]) {
 premotifmem.memorize(msg[1], chromatic.length*1);
-}
+
 
 
 if (msg[1] <= pianosectons[0]) {
@@ -229,6 +229,7 @@ if (minimotifSearch(minimotifsH.memory, miniM1H)) {
 
 
       // motif 2:
+
    if (compareMotif(motifmem.memory, motif2) == true) {
 console.log("motif 2 on");
   //  robot.typeString('~snippet1 = Ndef(\\acc, {|note=100, amp=0.03| SinOsc.ar([note.lag(1), note.lag(2)*3/2, note*2, note.lag(1.5)*4/3]) * amp}).play(0,2);');
@@ -329,6 +330,7 @@ robot.keyTap('enter', 'shift'); robot.keyTap('enter');
   }
 }
 
-} // fi tremolo
+    } // fi tremolo
 
+  } // fi note offs
  });
